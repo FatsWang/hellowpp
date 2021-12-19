f37f97de46e650cefed11d84b75d7550804c9f28
FatsWang <57435874+FatsWang@users.noreply.github.com>
2021-12-19 19:48:51 +0800

    全新版本

--- src/settings.ba549.js
+++
@@ -1,1 +0,0 @@
-window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:true,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/Map/UI_Main.fire",orientation:"",jsList:[],bundleVers:{resources:"04683",internal:"6e7b7",main:"1d182"}};
